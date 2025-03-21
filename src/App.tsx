a49110d3ca613cf7d0cb0f5dff4081e1ed25c58e
neelan2000 <neelan2000@gmail.com>
2025-03-21 15:10:48 +0530

    Update App.tsx
    Per user authorization

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ const client = generateClient<Schema>();
 
 function App() {
   const [todos, setTodos] = useState<Array<Schema["Todo"]["type"]>>([]);
-  const { signOut } = useAuthenticator();
+  const { user,signOut } = useAuthenticator();
   useEffect(() => {
     client.models.Todo.observeQuery().subscribe({
       next: (data) => setTodos([...data.items]),
@@ -22,6 +22,7 @@ function deleteTodo(id: string) {
   return (
     <main>
       <h1>My todos</h1>
+      <h1>{user?.signInDetails?.loginId}'s todos</h1>
       <button onClick={createTodo}>+ new</button>
       <ul>
         {todos.map((todo) => (

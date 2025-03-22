f12a0c9b456276aefc90544acb4f62d2524e0db9
neelan2000 <neelan2000@gmail.com>
2025-03-22 13:56:51 +0530

    Update main.tsx
    Redirect to sign up

--- src/main.tsx
+++ src/main.tsx
@@ -12,7 +12,7 @@ Amplify.configure(outputs);
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <Authenticator>
+    <Authenticator initialState="signUp">
     <App />
       </Authenticator>
   </React.StrictMode>

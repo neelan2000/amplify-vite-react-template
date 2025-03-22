a31db9323d86ba435aa1cd6263303c97b11d1348
neelan2000 <neelan2000@gmail.com>
2025-03-22 13:42:36 +0530

    Update App.tsx
    Added username

--- src/App.tsx
+++ src/App.tsx
@@ -21,7 +21,7 @@ function deleteTodo(id: string) {
   }
   return (
     <main>
-      <h1>My todos</h1>
+      <h1>Welcome {user.username}</h1>
       <h1>{user?.signInDetails?.loginId}'s todos</h1>
       <button onClick={createTodo}>+ new</button>
       <ul>
